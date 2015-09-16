4afc787ed49f711b1210bafa55d064b9d7f55930
stewart fraser <toepoke@hotmail.com>
2015-09-16 17:35:28 +0100

    Parsing the copy log in preparation for copying the fixed videos back to their original datasets.

--- copyVidsUpdatedParse.pl
+++ copyVidsUpdatedParse.pl
@@ -8,24 +8,56 @@ use Cwd;
 
 my $vidName;
 my $vidOriginalLocation;
-my $slotDir;
+my $binDir;
 
-my $fileName = "copyVidsUpdated.log";
-open my $fh, $fileName or die "Could not open file: $!";
+my $binExist;
+my $origExist;
 
-while (my $line = <$fh>) {
+my $vidCount=0;
+
+my $fileIn = "copyVidsUpdated.log";
+open (my $fh_in, "<", $fileIn) || die "Could not open file: $!";
+
+my $fileOut = "parse.log";
+open (my $fh_out, ">", $fileOut) || die "Couldn't open '".$fileOut."'for writing because: ".$!;
+
+while (my $line = <$fh_in>) {
+    $vidCount+=1;
 	chomp $line;
 	my @arr = split /=/, $line;
 	$vidName = $arr[0];
-	$vidOriginalLocation = $arr[1];
-	$slotDir = $arr[2];
-	say "$vidName\n$vidOriginalLocation\n$slotDir\n";
+	$vidOriginalLocation = "$arr[1]$arr[0]";
+	$binDir = $arr[2]."repaired\\"."$arr[0]";
 
-	# grau repaired output folder
+    if (-e $binDir){
+        $binExist="YYES";
+    }
+    else {
+        $binExist="NNO";
+    }
+    if (-e $vidOriginalLocation){
+        $origExist="YYES";
+    }
+    else {
+        $origExist="NNO";
+    }
 
-	# rename: move $slotName$vidName $slotName$newName
+    say $fh_out "$vidName";
+
+    #say $fh_out "$vidName";
+    #say $fh_out "$vidOriginalLocation";
+    #say $fh_out "$origExist";
+    #say $fh_out "$binDir";
+    #say $fh_out "$binExist\n";
+
+	# repaired output folder
+
+	# rename: move $binName$vidName $binName$newName
 	
-	# move $slotDir$newName $vidOriginalLocation
+	# move $binDir$newName $vidOriginalLocation
 	
 }
-close $fh;
+say "vidCount = $vidCount";
+close $fh_in;
+close $fh_out;
+exit;
